5bd5cb4f0d29f5eb979172c9e0083b7e990446e9
Alejandro Gonzalez <jgonzalezsole@appnexus.com>
2015-06-11 15:38:53 -0400

    added missing CSS

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 module.exports = {
   name: 'ember-hopscotch',
   included: function (app) {
-    app.import(app.bowerDirectory + "/hopscotch/dist/js/hopscotch.js");
+    app.import(app.bowerDirectory + '/hopscotch/dist/css/hopscotch.css');
+    app.import(app.bowerDirectory + '/hopscotch/dist/js/hopscotch.js');
   }
 };
